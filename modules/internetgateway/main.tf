517df47bc912b9fd5e116a7d5850da888c164d1f
mura651 <pivotal6051@gmail.com>
2025-01-03 20:36:44 +0900

    change internetgw

--- modules/internetgateway/main.tf
+++ modules/internetgateway/main.tf
@@ -1,3 +1,3 @@
 resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = var.vpc_id
 }
--- modules/internetgateway/variables.tf
+++ modules/internetgateway/variables.tf
@@ -1,3 +1,4 @@
-variable "name" {
+variable "vpc_id" {
   type        = string
+  description = "VPC ID to attach the IGW"
 }
