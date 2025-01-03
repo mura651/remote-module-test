1dbd83ecb5f38d261dd5d1aa60d55183f230d3d0
mura651 <pivotal6051@gmail.com>
2025-01-03 21:37:07 +0900

    change internetgw

--- modules/internetgateway/variables.tf
+++ modules/internetgateway/variables.tf
@@ -1,9 +1,1 @@
-variable "vpc_id" {
-  type        = string
-  description = "VPC ID to attach the IGW"
-}
-
-variable "name" {
-  description = "Name of the Internet Gateway"
-  type        = string
-}
+variable "name" {}
