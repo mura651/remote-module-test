ee57c4798df5fe92c6555a18b4fd385ed86a3010
mura651 <pivotal6051@gmail.com>
2025-01-03 21:35:00 +0900

    change internetgw

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,14 +1,2 @@
-variable "cidr_block" {
-  type        = string
-  description = "CIDR block for the VPC"
-}
-
-variable "name" {
-  type        = string
-  description = "Name tag for the VPC"
-}
-
-variable "vpc_id" {
-  type = string
-  description = "VPC ID to attach the internet gateway"
-}
+variable "cidr_block" {}
+variable "name" {}
